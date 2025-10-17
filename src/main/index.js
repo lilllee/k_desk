e99b7c025966a46a38315d0d37e997879e36f109
LillLEE <kopqwe147@gmail.com>
2025-10-17 17:54:43 +0900

    auto update test

--- src/main/index.js
+++ src/main/index.js
@@ -6,6 +6,8 @@ import testmgr from './testmgr'
 import createWsServer from './server'
 import path from 'path'
 import { defaultBrowserWindowOptions } from '@electron-toolkit/utils'
+import { autoUpdater } from 'electron-updater'
+import log from 'electron-log'
 
 
 app.setPath('userData', path.join(app.getPath('appData'), 'K-Desk'));
@@ -161,3 +163,29 @@ app.on('window-all-closed', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
+/* Updater ======================================================*/
+autoUpdater.on('checking-for-update', () => {
+  log.info('업데이트 확인 중...');
+});
+autoUpdater.on('update-available', (info) => {
+  log.info('업데이트가 가능합니다.');
+});
+autoUpdater.on('update-not-available', (info) => {
+  log.info('현재 최신버전입니다.');
+});
+autoUpdater.on('error', (err) => {
+  log.info('에러가 발생하였습니다. 에러내용 : ' + err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "다운로드 속도: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - 현재 ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  log.info(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  log.info('업데이트가 완료되었습니다.');
+});
+
+/* Electron =====================================================*/
--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -5,4 +5,3 @@ import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
-require('update-electron-app')()
